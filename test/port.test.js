d4ae5f1db568e9e006546bc1d9819bea0a7fb41b
yandeu <20306025+yandeu@users.noreply.github.com>
2022-06-16 15:11:34 +0200

    Update port.test.js

--- test/port.test.js
+++ test/port.test.js
@@ -9,12 +9,11 @@ beforeAll(async () => {
   port = server.port
 })
 
-test('should increase in use port by one', async done => {
+test('should increase in use port by one', async () => {
   const s = new Server()
   await s.serve(join(__dirname, '../example'), port, false)
   expect(port + 1).toBe(s.port)
   s.kill()
-  done()
 })
 
 afterAll(async () => {
